7d6e416b992ee8e7a60b6b31d1d7db49de71465a
Rey Martin <rmquintero60@gmail.com>
2023-06-08 18:23:37 -0600

    sesion ouput

--- src/app/dbz/componets/character/character.component.ts
+++ src/app/dbz/componets/character/character.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+import { Output } from '@angular/core';
 
 @Component({
   selector: 'app-add-character',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./character.component.css']
 })
 export class CharacterComponent {
+
+  @Output()
+  public onNewCharacter: EventEmitter<Character> = new EventEmitter();
+
+  public character: Character = {
+    name: '',
+    power: 0
+  };
+
+  emitCharacter():void {
+    console.log(this.character);
+    if( this.character.name.length === 0 ) return;
+
+    this.onNewCharacter.emit(this.character);
+
+    this.character.name = '';
+    this.character.power = 0;
+  }
 
 }
--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { MainPageComponent } from './pages/main-page.component';
 import { ListComponent } from './componets/list/list.component';
 import { CharacterComponent } from './componets/character/character.component';
 
+
 @NgModule({
-  imports: [],
+  imports: [CommonModule, FormsModule],
   exports: [MainPageComponent],
-  declarations: [MainPageComponent, ListComponent, CharacterComponent],
+  declarations: [MainPageComponent,ListComponent, CharacterComponent],
   providers: [],
 })
 export class DbzModule { }
